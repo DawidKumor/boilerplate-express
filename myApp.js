195368c62d7d4b92d27b5d18263badb73857cfce
DawidKumor <dawidkumor@gmail.com>
2026-05-12 00:46:25 +0200

    res.json added

--- myApp.js
+++ myApp.js
@@ -11,6 +11,9 @@ let app = express();
     }
 )
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"})
+})
 console.log("Hello World")
 
 
