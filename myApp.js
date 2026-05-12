bc235424b197bacf57a5130057c1f70a3ea5b262
DawidKumor <dawidkumor@gmail.com>
2026-05-12 19:33:41 +0200

    Add middleware to log method, path and IP for all requests

--- myApp.js
+++ myApp.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 let express = require('express');
 let app = express();
+
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
 /* app.get("/", 
    function(req, res) {
        res.send("Hello Express")
    } */
+
 const absolutePath = __dirname + "/views/index.html";
 app.get("/",
     function (req, res) {
         res.sendFile(absolutePath)
     }
 )
+
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/json", function (req, res) {
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({ "message": "Hello json".toUpperCase() })
